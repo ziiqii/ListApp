84e575fa65008c35d1dfb78e2ae8a5765d1f4931
Ziqi <ziiiqiii@gmail.com>
2022-06-12 14:30:22 +0800

    Finished list app

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
 import names from "./names";
 
+const namesAsObject = names.map((name) => {
+  return {
+    name: name,
+  };
+});
+
 export default function App() {
+  const renderItem = ({ item }) => {
+    return <View style={styles.listItem}>{item.name}</View>;
+  };
+
   return (
-    <ScrollView>
-      {names.map((name) => (
-        <Text>{name}</Text>
-      ))}
-      <StatusBar style="auto" />
-    </ScrollView>
+    <FlatList
+      data={namesAsObject}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.name}
+    />
   );
 }
 
@@ -18,5 +27,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  listItem: {
+    padding: 20,
+    margin: 10,
+    backgroundColor: "lightblue",
+    borderBottomColor: "#999",
+    borderBottomWidth: 1,
+    shadowColor: "black",
+    shadowRadius: 5,
+    shadowOpacity: 0.2,
+    shadowOffset: { x: 0, y: 3 },
   },
 });
